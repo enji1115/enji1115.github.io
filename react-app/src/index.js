83afa38d36eb6a4e22839b37be912fbf54e3a7a1
enji1115 <enji1560@naver.com>
2022-01-17 10:22:37 +0900

    220117 Set up React build before making component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Test from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +15,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// [실행방법]
+// 1. npm run start
+// 인데 이렇게 실행하면 React의 불필요한 기능까지 로드하여 소스가 무거워짐
+// 이를 해결하기 위해 빌드를 만듦
+// 2. npm run build
+// 이고 이를 실행하기 위해 serve라는 걸 다운받음(npm install -g serve)
+// -> npx serve -g build
+// 다운받은 serve를 사용하여 빌드를 한 번만 실행
