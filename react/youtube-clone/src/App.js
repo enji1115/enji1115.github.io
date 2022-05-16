9bc20e8e9a67967f1c219a2dfc50fe6642653104
Cho Eun Ji <jonji1115@gmail.com>
2022-05-16 16:22:47 +0900

    220516 Study React > Layout, useState

--- react/youtube-clone/src/App.js
+++ react/youtube-clone/src/App.js
@@ -1,74 +1,25 @@
 // src/App.js
-import './App.css';
-import youtubeData from './data/youtubeData.json';
+import React, { useState } from 'react';
+import Layout from './components/shared/Layout';
 
 function App() {
-  console.log('youtubeData : ', youtubeData);
-  console.log('youtubeData[data]', youtubeData['data']);
-  console.log('youtubeData[data][0]', youtubeData['data'][0]);
-  console.log('youtubeData[data][0]', youtubeData['data'][0].title);
-
-  // Quiz
-  const quiz_obj = youtubeData['data'][0];
-  let quiz_console = ``;
-  for(const value in quiz_obj){
-    quiz_console = `${value}: ${quiz_obj[value]}`;
-    switch (value) {
-      case 'id':
-        quiz_console += ' -> 영상의 링크 주소'
-        break;
-        
-      case 'title':
-        quiz_console += ' -> 영상의 제목'
-        break;
-        
-      case 'channelId':
-        quiz_console += ' -> 영상의 채널 주소'
-        break;
-        
-      case 'date':
-        quiz_console += ' -> 영상의 날짜'
-        break;
-        
-      case 'thumbnail':
-        quiz_console += ' -> 영상의 썸네일 이미지'
-        break;
-        
-      case 'description':
-        quiz_console += ' -> 영상의 설명'
-        break;
-        
-      case 'channelTitle':
-        quiz_console += ' -> 영상의 채널명'
-        break;
-        
-      case 'category':
-        quiz_console += ' -> 영상의 카테고리'
-        break;
-        
-      case 'viewCount':
-        quiz_console += ' -> 영상의 재생수'
-        break;
-        
-      case 'likeCount':
-        quiz_console += ' -> 영상의 좋아요수'
-        break;
-        
-      case 'channelUrl':
-        quiz_console += ' -> 영상의 채널 주소'
-        break;
-        
-      case 'channelThumbnail':
-        quiz_console += ' -> 영상의 채널 썸네일'
-        break;
-    
-      default:
-        break;
-    }
-    console.log(quiz_console);
+  const [toggle, setTog] = useState(true);
+  function onClick() {
+    setTog(!toggle);
   }
 
-  return <div>리액트로 데이터 불러오기</div>;
+  
+  return (
+    <Layout>
+      <div>콘텐츠!</div>
+      {true && <div>True일 때 출력되는 메세지</div>}
+      {false && <div>False일 때 출력되는 메세지</div>}
+
+      <button onClick={onClick}>tog</button>
+      {toggle===true && <div>True일 때 출력되는 메세지</div>}
+      {toggle===false && <div>False일 때 출력되는 메세지</div>}
+    </Layout>
+  );
 }
 
 export default App;
---
+++ react/youtube-clone/src/components/shared/Header.js
@@ -0,0 +1,8 @@
+// src/components/shared/Header.js
+import styles from './Header.module.css';
+
+function Header() {
+  return <div>헤더!</div>;
+}
+
+export default Header;
