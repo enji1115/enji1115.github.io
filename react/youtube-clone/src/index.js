44d6f75b5a76ed93633b34543a1deb4caae7e33a
Cho Eun Ji <jonji1115@gmail.com>
2022-05-17 21:26:43 +0900

    220517 Study React > Hosting

--- react/youtube-clone/src/index.js
+++ react/youtube-clone/src/index.js
@@ -2,21 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
 const root = document.getElementById('root');
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
