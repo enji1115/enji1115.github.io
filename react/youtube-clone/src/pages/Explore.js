7f87dc8bbb8d19b61b3baf79b2c72b7fa6234aff
Cho Eun Ji <jonji1115@gmail.com>
2022-05-17 11:09:54 +0900

    220517 Study React > Map

--- react/youtube-clone/src/pages/Explore.js
+++ react/youtube-clone/src/pages/Explore.js
@@ -1,13 +1,48 @@
 // src/pages/Explore.js
 import Layout from '../components/shared/Layout';
-// import styles from './Explore.module.css';
 import ContentsLayout from '../components/shared/ContentsLayout';
+import youtubeData from '../data/youtubeData.json';
+import ExploreCard from '../components/explore/ExploreCard';
 
 function Explore() {
+    // function mapFuncion(data, index) {
+    //     return (
+    //         <div>
+    //             {index}: {data}
+    //         </div>
+    //     );
+    // }
+
+    // function ExploreCard(data, index) {
+    //     return (
+    //         <div key={`explore-card-${index}`}>
+    //             {/* key 속성은 리턴되는 JSX의 최상단 태그에 부여 */}
+    //             <div>{index}.</div>
+    //             <div>제목 : {data.title}</div>
+    //             <div>업로더 : {data.channelTitle}</div>
+    //             <br />
+    //         </div>
+    //     );
+    // }
+
     return (
         <Layout activeMenu="explore">
             {/* <div className={styles.container}>* 탐색</div> */}
-            <ContentsLayout>컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠컨텐츠</ContentsLayout>
+            <ContentsLayout>
+                {/* {['모각코', '리액트', '유튜브', '클론코딩'].map(mapFuncion)}
+                <br/>
+                {mapFuncion('모각코', 0)}
+                {mapFuncion('리액트', 1)}
+                {mapFuncion('유튜브', 2)}
+                {mapFuncion('클론코딩', 3)}
+                <br/> */}
+
+                {/* {youtubeData['data'].map(ExploreCard)} */}
+                
+                {youtubeData['data'].map(function (data, index) {
+                    return <ExploreCard key={`explore-card-${index}`} data={data} />;
+                })}
+            </ContentsLayout>
         </Layout>
     );
 }
